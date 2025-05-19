ca57b9c8750ece38dbf1863635a60d2925721ed9
jaiasnani <105320108+jaiasnani@users.noreply.github.com>
2025-05-19 20:39:51 +0530

    Update ingest_agent.py

--- agents/ingest_agent.py
+++ agents/ingest_agent.py
@@ -2,8 +2,8 @@ import pandas as pd
 import requests
 from io import StringIO
 
-def ingest_csv_from_gdrive(gdrive_link):
-    file_id = gdrive_link.split("/d/")[1].split("/")[0]
+def ingest_csv_from_gdrive():
+    file_id = "1zLuphyJJGJoXZBmAnPqW8_Lxqw42KYbC"
     download_url = f"https://drive.google.com/uc?id={file_id}&export=download"
     response = requests.get(download_url)
     df = pd.read_csv(StringIO(response.text))
